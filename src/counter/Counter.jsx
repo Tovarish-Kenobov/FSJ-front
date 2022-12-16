93164234b6e8ff6b439802fd7d259d97dff95879
Tovarish-Kenobov <56266651+Tovarish-Kenobov@users.noreply.github.com>
2022-12-16 15:24:52 +0300

    Add files via upload

---
+++ src/counter/Counter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Hoc from "../hoc/Hoc";
+
+const Counter =(props)=>{
+    return(
+<div>
+    <button onClick={props.countDown} >
+        -
+
+    </button>
+
+    <span>{props.count}</span>
+
+    <button onClick={props.countUp} >
+        +
+    </button>
+</div>
+    )
+}
+
+export default Hoc(Counter)
